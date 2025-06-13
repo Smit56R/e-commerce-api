af766f3d5406ea18ee5bd3fb8e549edd7a9c3c07
Smit56R <82604082+Smit56R@users.noreply.github.com>
2025-06-13 22:58:35 +0530

    Learning create, update, delete request

--- src/main/java/com/codewithmosh/store/dtos/ProductDto.java
+++ src/main/java/com/codewithmosh/store/dtos/ProductDto.java
@@ -1,12 +1,12 @@
 package com.codewithmosh.store.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
-@AllArgsConstructor
-@Getter
+@Data
 public class ProductDto {
     private Long id;
     private String name;
