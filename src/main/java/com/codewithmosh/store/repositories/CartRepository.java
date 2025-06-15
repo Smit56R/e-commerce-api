8b4e20744774c786c91406e4550122ebb2777a13
Smit56R <82604082+Smit56R@users.noreply.github.com>
2025-06-15 23:22:06 +0530

    Add 'Create cart' endpoint

---
+++ src/main/java/com/codewithmosh/store/repositories/CartRepository.java
@@ -0,0 +1,10 @@
+package com.codewithmosh.store.repositories;
+
+import com.codewithmosh.store.entities.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface CartRepository extends CrudRepository<Cart, UUID> {
+    // Additional methods specific to Cart can be defined here if needed
+}
