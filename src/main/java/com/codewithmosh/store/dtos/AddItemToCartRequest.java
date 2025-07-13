f4cfc5b1bf71360249b4efaf911bb1988627d073
Smit56R <82604082+Smit56R@users.noreply.github.com>
2025-07-13 14:53:17 +0530

    Minor fix and add product.json

--- src/main/java/com/codewithmosh/store/dtos/AddItemToCartRequest.java
+++ src/main/java/com/codewithmosh/store/dtos/AddItemToCartRequest.java
@@ -1,12 +1,10 @@
 package com.codewithmosh.store.dtos;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AddItemToCartRequest {
-    @NotBlank(message = "Product ID is required")
-    @NotNull
+    @NotNull(message = "Product ID is required")
     private Long productId;
 }
