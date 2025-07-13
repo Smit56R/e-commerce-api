fdd7559522d85b7779256a8d6299be0897e547ed
Smit56R <82604082+Smit56R@users.noreply.github.com>
2025-07-13 16:14:25 +0530

    Add GET /orders

--- src/main/java/com/codewithmosh/store/repositories/OrderRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/OrderRepository.java
@@ -1,7 +1,18 @@
 package com.codewithmosh.store.repositories;
 
 import com.codewithmosh.store.entities.Order;
+import com.codewithmosh.store.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    @EntityGraph(attributePaths = "items.product")
+    @Query("SELECT o FROM Order o WHERE o.customer = :customer")
+    List<Order> getAllByCustomer(@Param("customer") User customer);
 }
